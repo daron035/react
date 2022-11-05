da16c52dd6ace7c5ac6c13b4e7e15f32501d6c88
daron035 <kamil249@mail.ru>
2022-11-05 11:21:16 +0500

    Список постов. Управляемый / неуправляемый инпут

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript", body: "Description" },
+    { id: 3, title: "Javascript", body: "Description" },
+  ]);
+  const [title, setTitle] = useState("");
+  const inputRef = useRef();
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(inputRef.current);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {/*Управляемый компонет*/}
+        {/*Управляемый компонет*/}
+        {/*Управляемый компонет*/}
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        {/*Неуправляемый компонент*/}
+        {/*Неуправляемый компонент*/}
+        {/*Неуправляемый компонент*/}
+        {/*<input ref={inputRef} type="text" />*/}
+        <MyInput ref={inputRef} type="text" placeholder="Описание" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов 1" />
     </div>
   );
 }
